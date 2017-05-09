a6a1cd26d145860b01371a132a92efd7cedb48df
Robert Jones <robert.g.jones@gmail.com>
2017-05-09 17:16:09 +0100

    tests plane land isnt undefined

---
+++ spec/FeatureSpec.js
@@ -0,0 +1,11 @@
+describe("Airport", function() {
+
+  var plane = new Plane();
+  var airport = new Airport();
+
+  it("can land a plane at a certain airport", function() {
+    plane.land(airport);
+    expect(airport.planes()).toContain(plane);
+  });
+
+});
--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,11 +1,10 @@
 describe("Plane", function() {
 
   var plane = new Plane();
-  var airport = new Airport();
 
-  it("can land a plane at a certain airport", function() {
-    plane.land(airport);
-    expect(airport.planes()).toContain(plane);
+  describe("#land", function() {
+      it("expects to receive land", function() {
+        expect(plane.land()).not.toBeUndefined();
+      });
   });
-
-});
+})
